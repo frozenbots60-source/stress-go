a2020fda081ceb205bc563ed32e9bad79fa20ec5
frozenbots60-source <frozenbots60@gmail.com>
2026-02-28 13:21:10 +0530

    Update main.go

--- main.go
+++ main.go
@@ -21,9 +21,9 @@ import (
 // ==========================================
 var (
 	SERVER_URL      = getEnv("TARGET_URL", "wss://kingclaimer.xyz:8443/")
-	TOTAL_CLIENTS   = 2           // Recommended to keep at 1 to avoid Cloudflare flags
-	MAX_WORKERS     = 2           
-	RECONNECT_DELAY = 3 * time.Second // Slower reconnect to avoid IP bans
+	TOTAL_CLIENTS   = 20           // Recommended to keep at 1 to avoid Cloudflare flags
+	MAX_WORKERS     = 20           
+	RECONNECT_DELAY = 5 * time.Second // Slower reconnect to avoid IP bans
 )
 
 // Worker Semaphore to limit max workers
