1e3c2f5c86f7535a180b766563228101142fb827
frozenbots60-source <frozenbots60@gmail.com>
2026-02-28 13:04:29 +0530

    Update main.go

--- main.go
+++ main.go
@@ -21,8 +21,8 @@ import (
 // ==========================================
 var (
 	SERVER_URL      = getEnv("TARGET_URL", "wss://kingclaimer.xyz:8443/")
-	TOTAL_CLIENTS   = 2000           // Recommended to keep at 1 to avoid Cloudflare flags
-	MAX_WORKERS     = 2000           
+	TOTAL_CLIENTS   = 2           // Recommended to keep at 1 to avoid Cloudflare flags
+	MAX_WORKERS     = 2           
 	RECONNECT_DELAY = 3 * time.Second // Slower reconnect to avoid IP bans
 )
 
